36822c7d000ad72cbe1b46ce5111fb0aa4cc0860
Matsuda Takumi <101240248+matsuda-tkm@users.noreply.github.com>
2022-06-07 23:52:12 +0900

    Update aimyon_generator.py

--- playground/aimyon_generator.py
+++ playground/aimyon_generator.py
@@ -2,35 +2,41 @@
 import MeCab
 import random
 
-kashi_list = pd.read_csv("kashi.csv")
-kashi_list = list(kashi_list["0"].values)
-tagger = MeCab.Tagger("-Owakati")
 
-# 単語、分かち書き、単語一覧
-words = []
-wakachi = []
-for k in kashi_list:
-  s = tagger.parse(k).split()
-  words += s
-  wakachi += [s]
-unique = list(set(words))
-
-# 隣接リスト
-ls = [[] for i in range(len(unique))]
-
-for k in wakachi:
-  for n,w in enumerate(k):
-    if n == len(k)-1:
-      continue
-    w1 = w
-    w2 = k[n+1]
-    ls[unique.index(w1)].append(unique.index(w2))
+# 歌詞を読み込み
+kashi_list = []
+with open("/content/drive/MyDrive/Colab Notebooks/あいみょん歌詞/kashi.txt", "r") as f:
+  for k in f:
+    kashi_list.append(k)
     
+# 分かち書きを読み込み
+wakachi = []
+with open("/content/drive/MyDrive/Colab Notebooks/あいみょん歌詞/wakachi.txt", "r") as f:
+  for song in f:
+    wakachi.append(song.split())
+
+# 単語一覧を読み込み
+unique = []
+with open("/content/drive/MyDrive/Colab Notebooks/あいみょん歌詞/word.txt", "r") as f:
+  for w in f:
+    unique.append(w.replace("\n",""))
+
+# 隣接リストを読み込む
+ls = []
+with open("/content/drive/MyDrive/Colab Notebooks/あいみょん歌詞/ls.txt", "r") as f:
+  for low in f:
+    ls.append([int(i) for i in low.split()])
+    
+
 # 初期番号
 num = random.randint(0,len(unique))
-# 文の長さ
-length = 30
+w = None
 
-for i in range(length):
+while w != ";":
   num = random.choice(ls[num])
-  print(unique[num], end="")
+  w = unique[num]
+  if "*" in w:
+    for i in range(len(w)):
+      print("")
+  else:
+    print(w, end="")
